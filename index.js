39f514022df87f96a567f2b2a61557f06ac98940
Akkrapol Kanpong <53117897+touchmylike2019@users.noreply.github.com>
2019-12-26 02:30:55 +0700

    heroku easy

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-    res.send("Hello World")
+    res.send(`Hello World ${process.env.DATA}`)
 })
 
 app.listen(process.env.PORT || 3000, () => console.log(`Server started on port 3000`))
