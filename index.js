e085a41132b618f175536eca8fae0567340222f1
Akkrapol Kanpong <53117897+touchmylike2019@users.noreply.github.com>
2019-12-26 23:01:30 +0700

    delete env

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
+
 const config = require('./config')
 const app = express()
+
 const MONGODB_URI = config.mogodburi
+
+app.use(bodyParser.json())
+
 app.get('/', (req, res) => {
-    res.send(`Hello World`)
+    res.json({
+        connect: 'pass'
+    })
 })
 
-mongoose.connect(MONGODB_URI, { useUnifiedTopology: true, useNewUrlParser: true }).then(
-    () => console.log(`Connect database success`),
-    err => console.log(`Connect database fail: ${err}`)
-)
+mongoose.connect(MONGODB_URI, { useUnifiedTopology: true, useNewUrlParser: true })
+    .then(
+        () => console.log(`Connect database success`),
+        err => console.log(`Connect database fail: ${err}`)
+    )
 
 app.listen(process.env.PORT || 3000, () => console.log(`Server started on port 3000`))
