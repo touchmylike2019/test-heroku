d613429ab02189fb16cfcdfe69d2f3efe038fe40
Akkrapol Kanpong <53117897+touchmylike2019@users.noreply.github.com>
2019-12-26 23:11:20 +0700

    add package

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const cors = require('cors')
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
 
@@ -7,6 +8,7 @@ const app = express()
 
 const MONGODB_URI = config.mogodburi
 
+app.use(cors())
 app.use(bodyParser.urlencoded({
     extended: true
 }))
