48107686b654c4e101db62aa5659a03d39b95083
Akkrapol Kanpong <53117897+touchmylike2019@users.noreply.github.com>
2019-12-26 23:17:30 +0700

    add controller

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
-// const DB_USERNAME = process.env.DB_USERNAME
-// const DB_PASSWORD = process.env.DB_PASSWORD
-const DB_USERNAME = 'bestculling'
-const DB_PASSWORD = 'tml2019'
+const DB_USERNAME = process.env.DB_USERNAME
+const DB_PASSWORD = process.env.DB_PASSWORD
+// const DB_USERNAME = 'bestculling'
+// const DB_PASSWORD = 'tml2019'
 module.exports = {
     mogodburi: `mongodb+srv://${DB_USERNAME}:${DB_PASSWORD}@datatest-cbti5.mongodb.net/test?retryWrites=true&w=majority`,
 }
